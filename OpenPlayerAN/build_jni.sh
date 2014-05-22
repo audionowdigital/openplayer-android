07eefa76dfb2a30802259a41e31cc46ea6fd2f40
radu.motisan <radhoo.tech@gmail.com>
2014-05-22 11:49:48 +0300

    dir change

--- OpenPlayerAN/build_jni.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cd jni
-ndk-build
--- OpenPlayerAN/jni/Android.mk
+++
@@ -1,5 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-#LOCAL_C_INCLUDE := $(LOCAL_PATH)/libogg/include
-
-include $(addprefix $(LOCAL_PATH)/, $(addsuffix /Android.mk, libogg libvorbis libvorbis-jni libopus libopus-jni ))
